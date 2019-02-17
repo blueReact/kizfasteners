e4250e1b92fcd1ad14bcde2e354e8cd71021d25b
rahul <rahul_r156@yahoo.com>
2019-02-17 15:48:47 +0530

    reverting codebase

--- app.js
+++ app.js
@@ -1,17 +1,41 @@
-var express = require('express');
-var app = express();
+const path = require('path');
+const express = require("express");
+const bodyParser = require('body-parser');
 
-port = process.env.PORT || 3000;
+// router
+const contactusRoutes = require('./routes/contactUs');
+const adminRoutes = require('./routes/admin');
+
+const port = process.env.PORT || 3000;
+const app = express();
+
+// parse epplication/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: false}));
+
+// parse application/json
+app.use(bodyParser.json());
+
+// serving static files
+app.use(express.static(path.join(__dirname, "public")));
+
+/*
+ * routes 
+ */
+
+// contactUs
+app.use('/', contactusRoutes);
+
+// admin
+app.use('/admin', adminRoutes);
 
 
-app.get('/', function(req, res){
-
-  res.send('Hello from cloud testing.');
-
+// error middleware
+app.use(function(req, res, next){
+  const statusCode = req.statusCode;
+  const status = req.status;
+  res.status(statusCode).send(status);
 });
-
 
 app.listen(port, function(){
   console.log('running on port', port);
 });
-
